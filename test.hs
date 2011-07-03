05b190be05252f92c228cda3d49db3e085e6c591
Roman Cheplyaka <roma@ro-che.info>
2011-07-03 15:30:00 +0300

    Test suite modifications
    Now we're testing over small alphabets like "a", "ab" and "abc".
    Also, added a test case from weighted regexp paper.

--- test.hs
+++ test.hs
@@ -1,43 +1,69 @@
+{-# LANGUAGE ViewPatterns #-}
 import RegExp
 import Reference
 import Control.Applicative
 import Test.SmallCheck
 import Data.Traversable
 
+-- Small alphabets as SmallCheck's series
+newtype A = A { a :: Char } deriving Show
+instance Serial A where
+    series = cons0 $ A 'a'
+    coseries = error "No coseries, sorry"
+
+newtype AB = AB { ab :: Char } deriving Show
+instance Serial AB where
+    series = cons0 (AB 'a') \/ cons0 (AB 'b')
+    coseries = error "No coseries, sorry"
+
+newtype ABC = ABC { abc :: Char } deriving Show
+instance Serial ABC where
+    series = cons0 (ABC 'a') \/ cons0 (ABC 'b') \/ cons0 (ABC 'c')
+    coseries = error "No coseries, sorry"
+
 re1 =
-    let one = pure 1 <* sym ()
-        two = pure 2 <* sym () <* sym ()
+    let one = pure 1 <* sym 'a'
+        two = pure 2 <* sym 'a' <* sym 'a'
     in (,) <$> (one <|> two) <*> (two <|> one)
 
 re2 = sequenceA $
-    [ pure 1 <* sym True <* sym True <|>
-      pure 2 <* sym True
-    , pure 3 <* sym False
-    , pure 4 <* sym False <|>
-      pure 5 <* sym True ]
+    [ pure 1 <* sym 'a' <* sym 'a' <|>
+      pure 2 <* sym 'a'
+    , pure 3 <* sym 'b'
+    , pure 4 <* sym 'b' <|>
+      pure 5 <* sym 'a' ]
 
 re3 = sequenceA $
     [ pure 0 <|> pure 1
-    , pure 1 <* sym True <* sym True <|>
-      pure 2 <* sym True
-    , pure 3 <* sym False <|> pure 6
+    , pure 1 <* sym 'a' <* sym 'a' <|>
+      pure 2 <* sym 'a'
+    , pure 3 <* sym 'b' <|> pure 6
     , fmap (+1) $
-      pure 4 <* sym False <|>
+      pure 4 <* sym 'b' <|>
       pure 7 <|>
-      pure 5 <* sym True ]
+      pure 5 <* sym 'a' ]
 
-re4 = sym True *> many (sym False) <* sym True
+re4 = sym 'a' *> many (sym 'b') <* sym 'a'
 
-re5 = (sym () <|> sym () *> sym ()) *> many (sym ())
+re5 = (sym 'a' <|> sym 'a' *> sym 'a') *> many (sym 'a')
 
-re6 = many (pure 3 <* sym () <* sym () <* sym () <|> pure 1 <* sym ())
+re6 = many (pure 3 <* sym 'a' <* sym 'a' <* sym 'a' <|> pure 1 <* sym 'a')
 
-prop re s = reference re s == s =~ re
+-- Regular expression from the weighted regexp paper.
+re7 =
+    let many_A_or_B = many (sym 'a' <|> sym 'b')
+    in (,) <$>
+        many ((,,,) <$> many_A_or_B <*> sym 'c' <*> many_A_or_B <*> sym 'c') <*>
+        many_A_or_B
+
+
+prop re f (map f -> s) = reference re s == s =~ re
 
 main = do
-   smallCheck 10 $ prop re1
-   smallCheck 10 $ prop re2
-   smallCheck 10 $ prop re3
-   smallCheck 10 $ prop re4
-   smallCheck 10 $ prop re5
-   smallCheck 10 $ prop re6
+   smallCheck 10 $ prop re1 a
+   smallCheck 10 $ prop re2 ab
+   smallCheck 10 $ prop re3 ab
+   smallCheck 10 $ prop re4 ab
+   smallCheck 10 $ prop re5 a
+   smallCheck 10 $ prop re6 a
+   smallCheck 7 $ prop re7 abc
