c38ff8f16f0cf1fde8350fd79b8f508810b09f7d
Roman Cheplyaka <roma@ro-che.info>
2012-04-18 01:31:23 +0300

    Supply one or two continuations (and save memory)

--- Text/Regex/Applicative/Compile.hs
+++ Text/Regex/Applicative/Compile.hs
@@ -6,10 +6,29 @@ import Prelude hiding ((.))
 import Text.Regex.Applicative.Types
 
 compile :: RE s a -> (a -> [Thread s r]) -> [Thread s r]
-compile e k = compile2 e k k
+compile e k = compile2 e (SingleCont k)
 
 infixr 9 .
 (f . g) x = f $! g x
+
+data Cont a = SingleCont !a | EmptyNonEmpty !a !a
+
+instance Functor Cont where
+    fmap f k =
+        case k of
+            SingleCont a -> SingleCont (f a)
+            EmptyNonEmpty a b -> EmptyNonEmpty (f a) (f b)
+
+emptyCont :: Cont a -> a
+emptyCont k =
+    case k of
+        SingleCont a -> a
+        EmptyNonEmpty a _ -> a
+nonEmptyCont :: Cont a -> a
+nonEmptyCont k =
+    case k of
+        SingleCont a -> a
+        EmptyNonEmpty _ a -> a
 
 -- The whole point of this module is this function, compile2, which needs to be
 -- compiled with -fno-do-lambda-eta-expansion for efficiency.
@@ -22,28 +41,30 @@ infixr 9 .
 --
 -- compile2 function takes two continuations: one when the match is empty and
 -- one when the match is non-empty. See the "Rep" case for the reason.
-compile2 :: RE s a -> (a -> [Thread s r]) -> (a -> [Thread s r]) -> [Thread s r]
+compile2 :: RE s a -> Cont (a -> [Thread s r]) -> [Thread s r]
 compile2 e =
     case e of
-        Eps -> \ke _kn -> ke $ error "empty"
-        Symbol i p -> \_ke kn -> [t kn] where
+        Eps -> \k -> emptyCont k $ error "empty"
+        Symbol i p -> \k -> [t $ nonEmptyCont k] where
           -- t :: (a -> [Thread s r]) -> Thread s r
           t k = Thread i $ \s ->
             if p s then k s else []
         App n1 n2 ->
             let a1 = compile2 n1
                 a2 = compile2 n2
-            in \ke kn ->
-            a1
-                -- empty
-                (\a1_value -> a2 (ke . a1_value) (kn . a1_value))
-                -- non-empty
-                (\a1_value -> a2 (kn . a1_value) (kn . a1_value))
+            in \k -> case k of
+                SingleCont k -> a1 $ SingleCont $ \a1_value -> a2 $ SingleCont $ k . a1_value
+                EmptyNonEmpty ke kn ->
+                    a1 $ EmptyNonEmpty
+                        -- empty
+                        (\a1_value -> a2 $ EmptyNonEmpty (ke . a1_value) (kn . a1_value))
+                        -- non-empty
+                        (\a1_value -> a2 $ EmptyNonEmpty (kn . a1_value) (kn . a1_value))
         Alt n1 n2 ->
             let a1 = compile2 n1
                 a2 = compile2 n2
-            in \ke kn -> a1 ke kn ++ a2 ke kn
-        Fmap f n -> let a = compile2 n in \ke kn -> a (ke . f) (kn . f)
+            in \k -> a1 k ++ a2 k
+        Fmap f n -> let a = compile2 n in \k -> a $ fmap (. f) k
         -- This is actually the point where we use the difference between
         -- continuations. For the inner RE the empty continuation is a
         -- "failing" one in order to avoid non-termination.
@@ -53,34 +74,37 @@ compile2 e =
                     case g of
                         Greedy -> continue ++ stop
                         NonGreedy -> stop ++ continue
-                threads b ke kn =
+                threads b k =
                     combine
-                        (a (\_ -> []) (\v -> let b' = f b v in threads b' kn kn))
-                        (ke b)
+                        (a $ EmptyNonEmpty (\_ -> []) (\v -> let b' = f b v in threads b' (SingleCont $ nonEmptyCont k)))
+                        (emptyCont k b)
             in threads b
-        Void n -> let a = compile2_ n in \ke kn -> a (ke ()) (kn ())
+        Void n -> let a = compile2_ n in \k -> a $ fmap ($ ()) k
 
-compile2_ :: RE s a -> [Thread s r] -> [Thread s r] -> [Thread s r]
+compile2_ :: RE s a -> Cont [Thread s r] -> [Thread s r]
 compile2_ e =
     case e of
-        Eps -> \ke _kn -> ke
-        Symbol i p -> \_ke kn -> [t kn] where
+        Eps -> \k -> emptyCont k
+        Symbol i p -> \k -> [t $ nonEmptyCont k] where
           -- t :: [Thread s r] -> Thread s r
           t k = Thread i $ \s ->
             if p s then k else []
         App n1 n2 ->
             let a1 = compile2_ n1
                 a2 = compile2_ n2
-            in \ke kn ->
-            a1
-                -- empty
-                (a2 ke kn)
-                -- non-empty
-                (a2 kn kn)
+            in \k ->
+                case k of
+                    SingleCont {} -> a1 $ SingleCont $ a2 k
+                    EmptyNonEmpty ke kn ->
+                        a1 $ EmptyNonEmpty
+                            -- empty
+                            (a2 $ EmptyNonEmpty ke kn)
+                            -- non-empty
+                            (a2 $ EmptyNonEmpty kn kn)
         Alt n1 n2 ->
             let a1 = compile2_ n1
                 a2 = compile2_ n2
-            in \ke kn -> a1 ke kn ++ a2 ke kn
+            in \k -> a1 k ++ a2 k
         Fmap _ n -> compile2_ n
         -- This is actually the point where we use the difference between
         -- continuations. For the inner RE the empty continuation is a
@@ -91,6 +115,6 @@ compile2_ e =
                     case g of
                         Greedy -> continue ++ stop
                         NonGreedy -> stop ++ continue
-                threads ke kn = combine (a [] (threads kn kn)) ke
+                threads k = combine (a $ EmptyNonEmpty [] (threads $ SingleCont $ nonEmptyCont k)) (emptyCont k)
             in threads
         Void n -> compile2_ n
