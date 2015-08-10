308a6e2edd7ef21e4e85ea6d4894de203d6573d5
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-08-10 13:22:24 +0300

    Add criterion suite with replace benchmark

--- benchmark/benchmark.hs
+++ benchmark/benchmark.hs
@@ -2,9 +2,18 @@ import Text.Regex.Applicative
 import Data.List
 import Data.Traversable
 import Data.Maybe
+import Criterion.Main
 
+regex :: RE Char String
 regex = sequenceA (replicate 500 $ sym 'a' <|> pure 'b') <* sequenceA (replicate 500 $ sym 'a')
 
-main = do
-    l <- getLine
-    print $ isJust $ l =~ regex
+matchRegex :: String -> Maybe String
+matchRegex = match regex
+
+a1000 :: String
+a1000 = replicate 1000 'a'
+
+main :: IO ()
+main = defaultMain
+  [ bgroup "basic" [ bench "all a" $ whnf matchRegex a1000 ]
+  ]
