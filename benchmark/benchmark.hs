d9ce1a5e39f695a563b84fca44ab27bd94ad5430
M Farkas-Dyck <strake888@gmail.com>
2017-12-15 21:06:33 -0800

    bench with cabal+criterion

--- benchmark/benchmark.hs
+++ benchmark/benchmark.hs
@@ -1,10 +1,11 @@
-import Text.Regex.Applicative
 import Data.List
 import Data.Traversable
 import Data.Maybe
 
+import Criterion.Main
+
+import Text.Regex.Applicative
+
 regex = sequenceA (replicate 500 $ sym 'a' <|> pure 'b') <* sequenceA (replicate 500 $ sym 'a')
 
-main = do
-    l <- getLine
-    print $ isJust $ l =~ regex
+main = defaultMain [bench "aaaaa" $ whnf (match regex) $ replicate 800 'a']
